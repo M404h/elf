d54bd86abbd83d5afb1318d6bed389ec14a0ad69
Maha <67692329+M404h@users.noreply.github.com>
2020-09-01 15:07:49 +0300

    Update elf.sh
    remove values

--- elf.sh
+++ elf.sh
@@ -7,5 +7,5 @@ helm repo update
 helm install elasticsearch elastic/elasticsearch --version=7.9.0 --namespace=elf --values toleration.yaml
 helm install fluent-bit fluent/fluent-bit --namespace=elf --values toleration.yaml
 helm install kibana elastic/kibana --version=7.9.0 --namespace=elf --set service.type=LoadBalancer --values toleration.yaml
-kubectl run random-logger --image=chentex/random-logger -n elf --values toleration.yaml
+kubectl run random-logger --image=chentex/random-logger -n elf
 kubectl apply -f ingress.yaml -n elf
