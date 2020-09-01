cc84a66807743d385f9d37b4aa2688b0dfc72526
Maha <67692329+M404h@users.noreply.github.com>
2020-09-01 14:33:11 +0300

    Update elf.sh
    added toleration command as value

--- elf.sh
+++ elf.sh
@@ -4,8 +4,8 @@ kubectl apply -f elf.namespace.yaml
 helm repo add elastic https://helm.elastic.co
 helm repo add fluent https://fluent.github.io/helm-charts
 helm repo update
-helm install elasticsearch elastic/elasticsearch --version=7.9.0 --namespace=elf
-helm install fluent-bit fluent/fluent-bit --namespace=elf
-helm install kibana elastic/kibana --version=7.9.0 --namespace=elf --set service.type=LoadBalancer
-kubectl run random-logger --image=chentex/random-logger -n elf
+helm install elasticsearch elastic/elasticsearch --version=7.9.0 --namespace=elf --values toleration.yaml
+helm install fluent-bit fluent/fluent-bit --namespace=elf --values toleration.yaml
+helm install kibana elastic/kibana --version=7.9.0 --namespace=elf --set service.type=LoadBalancer --values toleration.yaml
+kubectl run random-logger --image=chentex/random-logger -n elf --values toleration.yaml
 kubectl apply -f ingress.yaml -n elf
